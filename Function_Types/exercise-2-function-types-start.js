e845cbe930c2ac4298ce25f5febea43d66999fff
Silvio.Craparotta <craparottasilvio@gmail.com>
2023-03-18 18:38:46 +0100

    Function Types

---
+++ Function_Types/exercise-2-function-types-start.js
@@ -0,0 +1,26 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+// Add types to this function declaration.
+function doubleThePopulation(value) {
+    return value * 2;
+}
+// Correct or remove the function calls which cause type errors.
+doubleThePopulation(5);
+// doubleThePopulation("6.12"); Error type string
+doubleThePopulation(8526);
+// doubleThePopulation(false); Error type boolean
+// Alter this function so the `language2` parameter is optional.
+// Hint: Check `language2` is not `undefined` before passing it to `console.log()`.
+function languagesSpoken(country, language1, language2) {
+    console.log("The languages spoken in ".concat(country, " are:"));
+    console.log(language1);
+    if (language2 == "string") {
+        console.log(language2);
+    }
+}
+languagesSpoken("Colombia", "Spanish", "English");
+languagesSpoken("Greece", "Greek");
+languagesSpoken("New Zealand", "English", "Māori");
